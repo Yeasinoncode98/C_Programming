316e45324c69372ad967758f526e0e11049ad294
Yeasin Arafat <devoncode98@gmail.com>
2025-08-16 00:12:20 +0600

    Update function_SumNumbers_to_Nth_recursive_call.c

--- function_SumNumbers_to_Nth_recursive_call.c
+++ function_SumNumbers_to_Nth_recursive_call.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 int sumNumbers(int n){
-    if(n == 0){
-        return 0;
+    if(n != 0){
+         return n + sumNumbers(n-1);
     }
     
     else{
-        return n + sumNumbers(n-1);
+     
+        return n;
     }
 }
 
