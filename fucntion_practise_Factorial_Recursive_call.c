c06dcf50e508387d4ce6ada3ae906904d90d196c
Yeasin Arafat <devoncode98@gmail.com>
2025-08-15 23:47:30 +0600

    Create function_practise_Factorial_Recursive_call.c

---
+++ fucntion_practise_Factorial_Recursive_call.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    
+    else{
+        return n * factorial(n-1);
+    }
+}
+
+
+
+ int main() {
+
+ int num;
+ scanf("%d",&num);
+
+ int result = factorial(num);
+ printf("%d",result);
+
+    return 0;
+ }
+
+
+
+
+
